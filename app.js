757b3b3d22ce77b4f80a6be6749a800497e062da
Vraj Limbachiya <88069540+Vraj0109@users.noreply.github.com>
2023-01-02 13:54:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app =  express();
+app.use(bodyParser.urlencoded({extended:true}));
+const https = require("https");
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/",function(req,res){
+    var city  = req.body.c1;
+    console.log(city);
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=a14f80e9ccfa61542e7dada7195b3eff&units=metric";
+
+    https.get(url,function(responce) {
+        responce.on("data",function(data){
+            const wdata = JSON.parse(data);
+            console.log(wdata);
+            const temp = wdata.main.temp;
+            const condi = wdata.weather[0].icon;
+            console.log(condi);
+            const icon = "https://openweathermap.org/img/wn/"+condi+"@2x.png"
+            res.write("<h1>the temprature is "+temp+"</h1><br>");
+            res.write("<img src ="+icon+">");
+        })
+    })
+    console.log("the request has been made!");
+    
+})
+app.listen(process.env.PORT || 3000,function(){
+    console.log("the server is running on port 3000");
+})
